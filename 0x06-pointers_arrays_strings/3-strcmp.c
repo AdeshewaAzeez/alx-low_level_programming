fc6387b3975001d30dbd077d44af05aa7c25825b
AdeshewaAzeez <azeezfathia8@gmail.com>
2022-09-21 16:15:31 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,41 +1,20 @@
 #include "main.h"
 
 /**
- * _strcmp - function which compare two strings and
- * @si: first string
+ * _strcmp - string to compare
+ * @s1: first string
  * @s2: second string
- * Return:
- * returns zero if s1 == s2
- * returns negative number if s1 < s2
- * returns positive number if s1 > s2
+ *
+ * Return: value of string
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff = 0;
+	int i = 0;
 
-	while (i)
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			break;
-		else if (s1[i] == '\0')
-		{
-			diff = s2[i];
-			break;
-		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s1[i] != s2[i])
-		{
-			diff = s1[i] - s2[i];
-			break;
-		}
-		else
-			i++;
-
+		i++;
 	}
-	return (diff);
+	return (s1[i] - s2[i]);
 }
